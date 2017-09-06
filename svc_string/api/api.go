f83a778369dc1f72ff6dfca6ddae33db22f8c525
Kanitkorn S <k.sujautra@gmail.com>
2017-09-06 10:38:38 +0700

    Revert "Finally work. I'm gonna play Horizon ZERO DAWN now."
    This reverts commit 5203b43e96dac0c1c9da862bd2b10e50583a3b07.

--- svc_string/api/api.go
+++ svc_string/api/api.go
@@ -1,99 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
-	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/errors"
-	"golang.org/x/net/context"
-
 	proto "github.com/lukyth/try-go-micro/svc_string/proto"
+	"github.com/micro/go-micro"
+	api "github.com/micro/micro/api/proto"
+
+	"golang.org/x/net/context"
 )
 
-type uppercaseResult struct {
-	text string
-	err  error
-}
-
-type countResult struct {
-	count int32
-	err   error
-}
-
-// Service is a service
-type Service struct {
-	Client client.Client
+// String is struct
+type String struct {
+	Client proto.StringClient
 }
 
 // Uppercase is function
-func (s *Service) Uppercase(ctx context.Context, req *proto.UppercaseRequest, rsp *proto.UppercaseResponse) error {
+func (s *String) Uppercase(ctx context.Context, req *api.Request, rsp *api.Response) error {
 	log.Print("Received String.Uppercase API request")
 
-	// checkin and checkout date query params
-	text := req.Text
-
-	// make reqeusts for uppercase text
-	uppercaseResultCh := getUppercaseResult(ctx, s.Client, text)
-
-	// wait on uppercase reply
-	uppercaseResultReply := <-uppercaseResultCh
-	if err := uppercaseResultReply.err; err != nil {
-		return errors.InternalServerError("api.svc_string", err.Error())
+	response, err := s.Client.Uppercase(ctx, &proto.UppercaseRequest{
+		Text: req.Body,
+	})
+	if err != nil {
+		return err
 	}
 
-	rsp.Text = uppercaseResultReply.text
+	rsp.StatusCode = 200
+	b, _ := json.Marshal(map[string]string{
+		"text": response.Text,
+	})
+	rsp.Body = string(b)
+
 	return nil
-}
-
-// Count is function
-func (s *Service) Count(ctx context.Context, req *proto.CountRequest, rsp *proto.CountResponse) error {
-	log.Print("Received String.Count API request")
-
-	// checkin and checkout date query params
-	text := req.Text
-
-	// make reqeusts for count result
-	countResultCh := getCountResult(ctx, s.Client, text)
-
-	// wait on count reply
-	countResultReply := <-countResultCh
-	if err := countResultReply.err; err != nil {
-		return errors.InternalServerError("api.svc_string", err.Error())
-	}
-
-	rsp.Count = countResultReply.count
-	return nil
-}
-
-func getUppercaseResult(ctx context.Context, c client.Client, text string) chan uppercaseResult {
-	stringClient := proto.NewStringClient("svc_string", c)
-	ch := make(chan uppercaseResult, 1)
-
-	go func() {
-		res, err := stringClient.Uppercase(ctx, &proto.UppercaseRequest{
-			Text: text,
-		})
-
-		ch <- uppercaseResult{res.Text, err}
-	}()
-
-	return ch
-}
-
-func getCountResult(ctx context.Context, c client.Client, text string) chan countResult {
-	stringClient := proto.NewStringClient("svc_string", c)
-	ch := make(chan countResult, 1)
-
-	go func() {
-		res, err := stringClient.Count(ctx, &proto.CountRequest{
-			Text: text,
-		})
-
-		ch <- countResult{res.Count, err}
-	}()
-
-	return ch
 }
 
 func main() {
@@ -102,7 +42,11 @@ func main() {
 
 	service.Init()
 
-	proto.RegisterStringHandler(service.Server(), &Service{service.Client()})
+	service.Server().Handle(
+		service.Server().NewHandler(
+			&String{Client: proto.NewStringClient("svc_string", service.Client())},
+		),
+	)
 
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
